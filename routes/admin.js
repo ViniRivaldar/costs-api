a06b874396542cbfb4814f61bf7cf181aad9bfed
ViniRivaldar <vinicius-rivaldar@outlook.com>
2024-02-29 20:23:45 -0300

    ajustando rotas

--- routes/admin.js
+++ routes/admin.js
@@ -7,7 +7,7 @@ const {listar, postar, editar, deletar} = require('../controller/controls')
 
 router.get('/', listar)
 router.post('/add', postar)
-router.post('/edit/:id', editar)
-router.post('/delete/:id', deletar)
+router.put('/edit/:id', editar)
+router.delete('/delete/:id', deletar)
 
 module.exports = router
