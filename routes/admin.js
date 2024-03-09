97e1c9213db7af352c8b8f7ad3dd01167d512a91
ViniRivaldar <vinicius-rivaldar@outlook.com>
2024-03-09 19:04:55 -0300

    adicionando rota de categories

--- routes/admin.js
+++ routes/admin.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const router = express.Router()
 const mongoose = require('mongoose')
-const {listar, listarId, postar, editar, deletar} = require('../controller/controls')
+const {listar, listarCategorias, listarId, postar, editar, deletar} = require('../controller/controls')
 
 
 
 router.get('/', listar)
+router.get('/categories', listarCategorias)
 router.get('/:id', listarId)
 router.post('/add', postar)
 router.put('/edit/:id', editar)
